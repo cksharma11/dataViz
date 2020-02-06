f9b391fdc6806988c7a9cca2f520db65e9258105
Chandan Kumar <cksharma122@gmail.com>
2020-02-06 18:57:15 +0530

    D3 Class 1

--- public/js/buildings.js
+++ public/js/buildings.js
@@ -1,8 +1,40 @@
-const drawBuildings = (buildings)=>{
-  const toLine = b => `<strong>${b.name}</strong> <i>${b.height}</i>`;
-  document.querySelector('#chart-area').innerHTML = buildings.map(toLine).join('<hr/>');
-}
-const main = ()=>{
-  d3.json('data/buildings.json').then(drawBuildings);
-}
+const WIDTH = 400;
+const HEIGHT = 400;
+
+const drawBuildings = buildings => {
+  const maxHeight = _.maxBy(buildings, "height").height;
+
+  const x = d3
+    .scaleBand()
+    .range([0, WIDTH])
+    .domain(_.map(buildings, "name"))
+    .padding(0.3);
+
+  const y = d3
+    .scaleLinear()
+    .domain([0, maxHeight])
+    .range([0, HEIGHT]);
+
+  const chartArea = d3.select("#chart-area");
+  const svg = chartArea
+    .append("svg")
+    .attr("width", WIDTH)
+    .attr("height", HEIGHT);
+
+  const rectangles = svg.selectAll("rect").data(buildings);
+
+  const newReactangles = rectangles
+    .enter()
+    .append("rect")
+    .attr("y", 0)
+    .attr("x", b => x(b.name))
+    .attr("width", x.bandwidth)
+    .attr("height", b => y(b.height))
+    .attr("fill", "grey");
+};
+
+const main = () => {
+  d3.json("data/buildings.json").then(drawBuildings);
+};
+
 window.onload = main;
