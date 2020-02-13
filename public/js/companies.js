504f7fe044e5d1ccd5f73f72645494030a4347f5
Chandan Kumar <cksharma122@gmail.com>
2020-02-13 18:08:23 +0530

    Add transition

--- public/js/companies.js
+++ public/js/companies.js
@@ -1,18 +1,10 @@
-const drawCompanies = companies => {
-  const { Name, ...b } = _.first(companies);
-  const fields = _.keys(b);
-  let field = _.first(fields);
+const chartSize = { width: 800, height: 600 };
+const margin = { left: 100, right: 10, top: 10, bottom: 150 };
+const width = chartSize.width - (margin.left + margin.right);
+const height = chartSize.height - (margin.top + margin.bottom);
 
-  setInterval(() => {
-    fields.push(fields.shift());
-    field = _.first(fields);
-  }, 2000);
-
+const drawCompanies = (companies, field) => {
   const maxHeight = _.maxBy(companies, field)[field];
-  const chartSize = { width: 800, height: 600 };
-  const margin = { left: 100, right: 10, top: 10, bottom: 150 };
-  const width = chartSize.width - (margin.left + margin.right);
-  const height = chartSize.height - (margin.top + margin.bottom);
 
   const x = d3
     .scaleBand()
@@ -34,9 +26,8 @@ const drawCompanies = companies => {
 
   const xAxis = d3.axisBottom(x);
 
-  const chartArea = d3.select("#chart-area");
+  const chartArea = d3.select("#chart-area svg");
   const svg = chartArea
-    .append("svg")
     .attr("width", chartSize.width)
     .attr("height", chartSize.height);
 
@@ -84,11 +75,59 @@ const drawCompanies = companies => {
     .attr("fill", b => c(b.Name));
 };
 
+const percentageFormat = d => `${d}%`;
+const kCroresFormat = d => `${d / 1000}k Cr ₹`;
+
+const formats = {
+  MarketCap: kCroresFormat,
+  DivYld: percentageFormat,
+  ROCE: percentageFormat,
+  QNetProfit: percentageFormat,
+  QSales: percentageFormat
+};
+
+const updateCompanies = function(companies, fieldName) {
+  const svg = d3.select("#chart-area svg");
+  svg.select(".y.axis-label").text(fieldName);
+
+  const y = d3
+    .scaleLinear()
+    .domain([0, _.maxBy(companies, fieldName)[fieldName]])
+    .range([height, 0]);
+
+  const yAxis = d3
+    .axisLeft(y)
+    .tickFormat(formats[fieldName])
+    .ticks(10);
+
+  svg.select(".y-axis").call(yAxis);
+  svg
+    .selectAll("rect")
+    .transition()
+    .duration(1000)
+    .ease(d3.easeLinear)
+    .attr("y", c => y(c[fieldName]))
+    .attr("height", c => y(0) - y(c[fieldName]));
+};
+
+const updateCompaniesBars = companies => {
+  const { Name, ...b } = _.first(companies);
+  const fields = _.keys(b);
+  let field = _.first(fields);
+  drawCompanies(companies, field);
+
+  setInterval(() => {
+    fields.push(fields.shift());
+    field = _.first(fields);
+    updateCompanies(companies, field);
+  }, 2000);
+};
+
 const main = () => {
   d3.csv("data/companies.csv", ({ Name, ...rest }) => {
     Object.keys(rest).forEach(c => (rest[c] = +rest[c]));
     return { Name, ...rest };
-  }).then(drawCompanies);
+  }).then(updateCompaniesBars);
 };
 
 window.onload = main;
